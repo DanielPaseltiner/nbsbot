81ec6b9c7db8867776458878d010233c991b8cf7
Daniel Paseltiner <daniel.paseltiner@maine.gov>
2021-12-18 14:17:19 -0500

    Add Pandas import to nbsdriver.py.

--- nbsdriver.py
+++ nbsdriver.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
 from bs4 import BeautifulSoup
+import pandas as pd
 import sys
 
 
